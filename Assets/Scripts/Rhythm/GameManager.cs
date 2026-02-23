ff3de8369551b3ad8baac93cda555f8c4fe5ae3d
Amneet Singh <91922146+SinghAmneet@users.noreply.github.com>
2026-02-23 12:57:46 -0500

    Attack animation updated

---
+++ Assets/Scripts/Rhythm/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource music;
+    public bool startPlaying;
+    public BeatScroller bs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
