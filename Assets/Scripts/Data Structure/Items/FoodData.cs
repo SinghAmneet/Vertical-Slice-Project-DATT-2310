5677f62e36b42e3fa3c2e1dc56018321e8c72e14
shawn m <shawnm14@my.yorku.ca>
2026-03-09 18:14:40 -0400

    Update food stat display
    Food stats will now only show when hovering over the item in the hotbar

--- Assets/Scripts/Data Structure/Items/FoodData.cs
+++ Assets/Scripts/Data Structure/Items/FoodData.cs
@@ -17,6 +17,21 @@ public class FoodData : ItemData
     public Types type;
     public List<Stat> stats = new();
 
+    private List<Color> colors = new()
+    {
+        Color.green,
+        Color.blue,
+        Color.red,
+        Color.yellow,
+        Color.white,
+        Color.gray,
+    };
+
+    public Color GetColor()
+    {
+        return colors[(int) type];
+    }
+
     // when the player consumes the food
     public override void Use(GameObject plr)
     {
