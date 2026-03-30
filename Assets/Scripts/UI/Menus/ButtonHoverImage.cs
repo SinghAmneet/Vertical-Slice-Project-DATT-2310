79cab1e1c7aa316305d5e42dfd7e564bae790722
ngan-h <174763938+ngan-h@users.noreply.github.com>
2026-03-29 23:41:17 -0400

    mainSceneUpd
    Prefab
    Texture
    Pot
    RecipeButton

---
+++ Assets/Scripts/UI/Menus/ButtonHoverImage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHoverImage : MonoBehaviour
+{
+    public Image targetImage;
+    public Sprite normalSprite;
+    public Sprite hoverSprite;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        targetImage.sprite = hoverSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        targetImage.sprite = normalSprite;
+    }
+}
